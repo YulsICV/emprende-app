05a2450fdcb4acc2e50abb7e8aacf41f1c8aa158
YulsICV <yulivcrvs05@gmail.com>
2026-03-10 13:50:57 -0600

    fix varias paginas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./AuthProvider.jsx"; 
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
